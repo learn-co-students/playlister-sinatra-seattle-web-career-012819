2259565ae60408abc8203e7d0086d61d105315f8
github username <github email address>
2019-02-21 13:37:57 -0800

    fixing seed file again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-<<<<<<< HEAD
-require 'all'
-
 LibraryParser.parse 
-
-
-=======
-LibraryParser.parse
->>>>>>> 1469fe3e70d3c018cc511abd3125e599b5ea4f7f
