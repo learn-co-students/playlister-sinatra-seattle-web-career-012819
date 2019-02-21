a62786a860ef5e5dc7cfacdce43481bcdc7bbd25
github username <github email address>
2019-02-21 15:30:16 -0800

    built genre index page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,12 @@
 class GenresController < ApplicationController
-   
-   get '/genres' do 
-      erb :index 
-   end 
 
-   get '/genres/:name' do 
-      erb :show 
-   end 
+   get '/genres' do
+     @genre_list = Genre.all
+      erb :'genres/index'
+   end
+
+   get '/genres/:name' do
+      erb :show
+   end
 
 end
