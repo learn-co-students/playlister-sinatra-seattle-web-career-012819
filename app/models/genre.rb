314928c66d60913df5e3a20ec90cc2908c2a1f9d
github username <crishanks12@gmail.com>
2019-02-21 14:10:39 -0800

    minor fix to relationships, added slug and find_by_slug methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,16 @@
 class Genre < ActiveRecord::Base
     has_many :song_genres
-    has_many :songs, through: :song_genre
+    has_many :songs, through: :song_genres
     has_many :artists, through: :songs
+
+    def slug
+        self.name.downcase.split(' ').join('-')
+    end
+
+    def self.find_by_slug(slug)
+        original_name = slug.titleize
+        self.all.find do |genre|
+            genre.name == original_name
+        end
+    end
 end
