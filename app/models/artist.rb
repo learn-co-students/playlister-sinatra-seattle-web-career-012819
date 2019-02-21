689085106c09eaee2f329fd519112af34db52042
github username <crishanks12@gmail.com>
2019-02-21 14:07:55 -0800

    added slug and find_by_slug methods to songs class

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,7 +5,7 @@ class Artist < ActiveRecord::Base
     has_many :genres, through: :songs
 
     def slug
-        slugged_name = self.name.downcase.split(' ').join('-')
+        self.name.downcase.split(' ').join('-')
     end
 
     def self.find_by_slug(slug)
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,15 @@ class Song < ActiveRecord::Base
     belongs_to :artist
     has_many :genres, through: :song_genres
     has_many :song_genres
+
+    def slug
+        self.name.downcase.split(' ').join('-')
+    end
+
+    def self.find_by_slug(slug)
+        original_name = slug.titleize
+        self.all.find do |song|
+            song.name == original_name
+        end
+    end
 end
