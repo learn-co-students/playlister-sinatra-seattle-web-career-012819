bb3e21df7f49fe6035be6a23e2188525b96055bd
github username <github email address>
2019-02-21 17:04:23 -0800

    Solving dynamic route errors.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,18 @@ class SongController < ApplicationController
   end #end of get '/songs' path
 
   get '/songs/new' do
+    @genres = Genre.all
     erb :new
+  end
+
+  post '/songs/create' do
+    artist = Artist.find_or_create_by(name: params["Artist Name"])
+    song = Song.create(name: params[:Name], artist_id: artist.id)
+    genres = params.keys.select { |genre| params[genre]=="on" }
+    genres.map! { |genre| Genre.find_by(name: genre) }
+    genres.each { |genre| SongGenre.create(song_id: song.id, genre_id: genre.id) }
+    redirect "/songs/#{song.slug}"
+    #binding.pry
   end
 
   get '/songs/:slug' do
