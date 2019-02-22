e0a4857703bac9397c2e956c36043641dbc69094
github username <crishanks12@gmail.com>
2019-02-21 17:19:01 -0800

    genres/:slug and genres routes

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,12 @@
 class GenresController < ApplicationController
+    set :views, 'app/views/genres/'
 
-    get '/genre' do
+    get '/genres' do
         @genres = Genre.all
         erb :index
+    end
+
+    get '/genres/:slug' do
+        erb :show
     end
 end
