af845d6dc53ca1df899dbc6db148c4186d053066
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-21 16:41:01 -0800

    Def routes & templates for creating a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,12 +6,21 @@ class SongsController < ApplicationController
     erb :index
   end
 
-  get 'songs/new' do
+  get '/songs/new' do
+    @genres = Genre.all
     erb :new
   end
 
   get '/songs/:id' do
     @song = Song.find(params[:id])
     erb :show
+  end
+
+  post '/songs' do
+    @new_song = Song.create(name: params[:song_name])
+    @new_song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @new_song.genres = params[:genres].map { |elmt| Genre.find(elmt.to_i) }
+    @new_song.save
+    redirect "/songs/#{@new_song.id}"
   end
 end
