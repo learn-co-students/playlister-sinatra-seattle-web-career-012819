25edcf451ab02275ca05aebd8b8282ddedaf34a9
github username <github email address>
2019-02-21 17:01:19 -0800

    built genre show page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,6 +8,7 @@ class GenresController < ApplicationController
    get '/genres/:slug' do
      @genre = Genre.find_by_slug(params[:slug])
      @songs = @genre.songs
+     @artists = @genre.artists
       erb :'genres/show'
    end
 
