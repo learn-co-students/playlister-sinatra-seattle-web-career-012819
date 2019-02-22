3d89356bab52decd070ab02251f23079fae22d08
github username <github email address>
2019-02-22 11:52:42 -0800

    finished project

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
 class SongsController < ApplicationController
+ use Rack::Flash
+
+
 
   get '/songs/new' do 
     @songs = Song.all 
@@ -14,25 +17,49 @@ class SongsController < ApplicationController
   end
 
   get '/songs' do
+
     @songs = Song.all
     erb :'songs/index'
   end
 
   post '/songs' do 
-    binding.pry
-    @song = Song.create(params)
-    @slug = @song.slug
-    redirect '/songs/@slug'
+    @artist = Artist.find_or_create_by(name: params[:song][:artist_name])
+    song_name = params[:song][:name]
+    song_hash = {}
+    song_hash[:artist_id] = @artist.id
+    song_hash[:name] = song_name
+    @song = Song.create(song_hash)
+    params[:song][:genres].each do |element|
+      @genre = Genre.find_by(id: element[:name])
+      @song.genres << @genre
+      @song.save
+    end
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
+    @genres = Genre.all 
+    @songs = Song.all 
+    @artists = Artist.all
+    @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
-    binding.pry
-    @artist.update(params[:name])
-    redirect '/songs/show'
+    @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.artist = @artist
+    @song.genres.clear
+    @song.save
+    params[:genres].each do |element|
+      @genre = Genre.find_by(id: element[:name])
+      @song.genres << @genre
+      @song.save
+    end
+    flash[:message] = "Successfully updated song."
+    redirect to ("/songs/#{@song.slug}")
   end
 
 end
