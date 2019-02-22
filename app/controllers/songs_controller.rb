51df87b68cb8cc7631ea000555fdfeb9cfe1d297
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-22 09:12:48 -0800

    Build out song/edit page & initial def of slug methods

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,6 +21,23 @@ class SongsController < ApplicationController
     @new_song.artist = Artist.find_or_create_by(name: params[:artist_name])
     @new_song.genres = params[:genres].map { |elmt| Genre.find(elmt.to_i) }
     @new_song.save
+
     redirect "/songs/#{@new_song.id}"
   end
+
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    @genres = Genre.all
+    erb :edit
+  end
+
+  patch '/songs/:id' do
+    @song = Song.find(params[:id])
+    @song.genres = params[:genres].map { |elmt| Genre.find(elmt.to_i) } if params[:genres]
+    @song.artist = params[:artist] if !(params[:artist].empty?)
+    @song.save
+
+    redirect "/songs/#{@new_song.id}"
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    new_name = self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def find_by_slug(slug)
+    slug
+    Song.find_by(name:)
+  end
 end
+
+# remove special characters
+# replace spaces with dashes
